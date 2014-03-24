4cc1c8f4740abe78d4186b961609d1bbdf009831
Shun Hotta <stars.big2men@gmail.com>
2014-03-24 19:33:35 +0900

    Change PluginRepository to not Singleton class

--- src/Toiee/HaikMarkdown/Plugin/Repositories/PluginRepository.php
+++ src/Toiee/HaikMarkdown/Plugin/Repositories/PluginRepository.php
@@ -5,44 +5,11 @@ use Toiee\HaikMarkdown\Plugin\PluginCounter;
 
 class PluginRepository implements PluginRepositoryInterface {
 
-    private static $instance = null;
-
     protected $repositories;
 
-    private function __construct()
-    {
-        $this->initialize();
-    }
-
-    /**
-     * Clean plugin repositories.
-     *
-     * @return PluginRepository $this for method chain
-     */
-    public function clean()
+    public function __construct()
     {
         $this->repositories = array();
-
-        return $this;
-    }
-
-    /**
-     * Initialize this instance.
-     * Clear PluginRepositoryInterface array and register Basic/Bootstrap plugin repositories.
-     *
-     * @return PluginRepository $this for method chain
-     */
-    public function initialize()
-    {
-        $this->clean();
-
-        $this->register(
-            new BasicPluginRepository
-        )->register(
-            new BootstrapPluginRepository
-        );
-
-        return $this;
     }
 
     /**
@@ -56,20 +23,6 @@ class PluginRepository implements PluginRepositoryInterface {
         array_unshift($this->repositories, $repository);
 
         return $this;
-    }
-
-    /**
-     * Returns the singleton instance
-     *
-     * @return PluginRepository
-     */
-    public static function getInstance()
-    {
-        if (null === self::$instance) {
-            self::$instance = new self();
-        }
-
-        return self::$instance;
     }
 
     /**
