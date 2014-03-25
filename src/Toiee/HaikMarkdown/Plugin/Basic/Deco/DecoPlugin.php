ff3c33263a5f1d0dbdb4511c2b8bb07ddef407ff
Shun Hotta <stars.big2men@gmail.com>
2014-03-25 15:33:14 +0900

    Add plugins

---
+++ src/Toiee/HaikMarkdown/Plugin/Basic/Deco/DecoPlugin.php
@@ -0,0 +1,73 @@
+<?php
+namespace Toiee\HaikMarkdown\Plugin\Basic\Deco;
+
+use Toiee\HaikMarkdown\Plugin\Plugin;
+
+class DecoPlugin extends Plugin {
+
+    /**
+     * inline call via HaikMarkdown &plugin-name(...){...};
+     * @params array $params
+     * @params string $body when {...} was set
+     * @return string converted HTML string
+     * @throws RuntimeException when unimplement
+     */
+    function inline($params = array(), $body = '')
+    {
+        $strong = false;
+        $ccnt = 0;
+        foreach ($params as $v)
+        {
+    		if( is_numeric($v) )
+    		{
+    			$size = $v.'px';
+    		}
+    		else if (preg_match('/^(\d|\.)/', $v))
+    		{
+    			$size = $v;
+    		}
+    		else if (preg_match('/small|medium|large/', $v))
+    		{
+    			$size = $v;
+    		}
+    		else if ($v=='bold' || $v=='b' ){
+    			$strong = true;
+    		}
+    		else if ($v=='underline' || $v=='u')
+    		{
+    			$underline = 'text-decoration:underline;';
+    		}
+    		else if ($v=='italic' || $v=='i')
+    		{
+    			$italic = 'font-style:italic;';
+    		}
+    		else if (preg_match('/^(#[0-9a-f]{3}|#[0-9a-f]{6}|[a-z-]+)$/i', $v))
+    		{
+    			$color[$ccnt] = $v;
+    			$ccnt++;
+    		}
+    		else if($v==''){
+    			$color[$ccnt] = 'inherit';
+    			$ccnt++;
+    		}
+        }
+        
+        $style = array();        
+        if (isset($size)) $style[] = "font-size:{$size};";
+        if (isset($color[0]) && $color[0]!='') $style[] = "color:{$color[0]};";
+        if (isset($color[1]) && $color[1]!='') $style[] = "background-color:{$color[1]};";
+        if (isset($underline)) $style[] = $underline;
+        if (isset($italic)) $style[] = $italic;
+        $style = join('', $style);
+        $style = trim($style, " \t\n\r\0\x0B;");
+        if ($style != '') $style = ' style="'.$style.'"';
+        
+        if ($strong)
+        {
+            $body = '<strong>'.$body.'</strong>';
+        }
+	
+        return '<span class="haik-plugin-deco"'.$style.'>'.$body.'</span>';
+    }
+
+}
