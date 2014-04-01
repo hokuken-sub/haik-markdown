252b20a4d92f92fb67cee194aab4acb5259e51af
masamori <r.masamori@gmail.com>
2014-04-01 17:41:11 +0900

    Change extends PluginRepostiory

--- src/Toiee/HaikMarkdown/Plugin/FlatUI/PluginRepository.php
+++ src/Toiee/HaikMarkdown/Plugin/FlatUI/PluginRepository.php
@@ -3,17 +3,60 @@ namespace Toiee\HaikMarkdown\Plugin\FlatUI;
 
 use Michelf\MarkdownInterface;
 use Toiee\HaikMarkdown\Plugin\Repositories\AbstractPluginRepository;
+use Toiee\HaikMarkdown\Plugin\Repositories\BootstrapPluginRepository;
 
 class PluginRepository extends AbstractPluginRepository {
 
-    /** @var boolean loadable compatible plugins */
-    protected $compatibleWithBootstrap;
+    protected $bootstrap_repository;
 
-    public function __construct(MarkdownInterface $parser, $compatible_with_bootstrap = false)
+    public function __construct(MarkdownInterface $parser)
     {
         parent::__construct($parser);
+        $this->bootstrap_repository = new BootstrapPluginRepository($parser);
         $this->repositoryPath = __DIR__;
-        $this->compatibleWithBootstrap = $compatible_with_bootstrap;
+    }
+
+    /**
+     * plugin $id is exists?
+     * @params string $id plugin id
+     * @return boolean
+     */
+    public function exists($id)
+    {
+        if (class_exists($this->getClassName($id), true))
+        {
+            return true;
+        }
+
+        if ($this->bootstrap_repository->exists($id))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * load Plugin by id
+     * @params string $id plugin id
+     * @return \Toiee\HaikMarkdown\Plugin\PluginInterface The Plugin
+     * @throws InvalidArgumentException when $id was not exist
+     */
+    public function load($id)
+    {
+        if (class_exists($this->getClassName($id), true))
+        {
+            $class_name = $this->getClassName($id);
+            return new $class_name($this->parser);
+        }
+
+        return $this->bootstrap_repository->load($id);
+    }
+
+    public function getAll()
+    {
+        $plugins = array_merge($this->bootstrap_repository->getAll(), parent::getAll());
+        return $plugins;
     }
 
     protected function getClassName($id)
