e441f6c50326d82f7ac43f845754b6331d4ddbc6
Keishi Tanaka <kc@toiee.jp>
2014-04-02 17:45:26 +0900

    Add description for convert plugin

--- src/Toiee/HaikMarkdown/Plugin/Bootstrap/Image/ImagePlugin.php
+++ src/Toiee/HaikMarkdown/Plugin/Bootstrap/Image/ImagePlugin.php
@@ -42,6 +42,13 @@ class ImagePlugin extends Plugin {
         return $this->parseParams()->renderView();
     }
 
+    /**
+     * convert call via HaikMarkdown {#plugin-name}
+     * @params array $params
+     * @params string $body when :::\n...\n::: was set
+     * @return string converted HTML string
+     * @throws RuntimeException when unimplement
+     */
     public function convert($params = array(), $body = '')
     {
         $this->block = true;
