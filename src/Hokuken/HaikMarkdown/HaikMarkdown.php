7384e9d006957ccadf75662460123bdd83f1cfb1
Shun Hotta <stars.big2men@gmail.com>
2014-04-28 16:04:46 +0900

    Release v0.6.0

--- src/Hokuken/HaikMarkdown/HaikMarkdown.php
+++ src/Hokuken/HaikMarkdown/HaikMarkdown.php
@@ -8,7 +8,7 @@ use Hokuken\HaikMarkdown\Plugin\SpecialAttributeInterface;
 
 class HaikMarkdown extends MarkdownExtra {
 
-    const VERSION = '0.5.1';
+    const VERSION = '0.6.0';
 
     /** @var boolean is parser running for prevent recursively parse */
     protected $running;
